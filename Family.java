6ecdf957509afbfa4361330c59e67eda21357300
Nick-Marzullo <nmarzullo@protonmail.ch>
2019-06-07 18:28:32 -0400

    Created new classes for families and individuals.
    I am having some massive issues with git.  I'll keep everyone updated

---
+++ Family.java
@@ -0,0 +1,18 @@
+//JMJ
+import java.util.*;
+/**
+ *
+ * @author Nick Marzullo
+ */
+public class Family {
+    public String id;
+    public String marriageDate;
+    public String divorceDate;
+    public String husbandId;
+    public String husbandName;
+    public String wifeId;
+    public String wifeName;
+    public ArrayList<String> ChildrenIds = new ArrayList<>();
+    
+    
+}
---
+++ Individual.java
@@ -0,0 +1,15 @@
+//JMJ
+
+/**
+ *
+ * @author Nick Marzullo
+ */
+public class Individual {
+    public String id;
+    public String name;
+    public String sex;
+    public String birthDate;
+    public String deathDate;
+    public String childIn;
+    public String spouseIn;
+}
