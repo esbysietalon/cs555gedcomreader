9b009abda588809f9cc4ea3b1b669b35b5e22929
talonjoaquin <jtalon@stevens.edu>
2019-06-24 15:45:24 -0400

    testing multiple tests

---
+++ TestFAIL.java
@@ -0,0 +1,17 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Test01 {
+
+	@Test
+	public void Test_US01() {
+		assertTrue(false);
+	}
+}
