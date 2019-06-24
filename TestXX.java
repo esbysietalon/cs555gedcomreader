1f5c1312fd4c7c64740289a6ba74ba8207e3f59b
talonjoaquin <jtalon@stevens.edu>
2019-06-24 15:11:12 -0400

    further testing on autotester

---
+++ TestXX.java
@@ -0,0 +1,17 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestXX {
+
+	@Test
+	public void Test_USXX() {
+		assertFalse(true);
+	}
+}
