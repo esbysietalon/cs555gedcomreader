0ec0fdc2bca1a97f051e80ad6789bcc93870f385
talonjoaquin <jtalon@stevens.edu>
2019-06-24 15:49:23 -0400

    rearranged tests

--- Test00.java
+++ Test00.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestFAIL {
+public class Test00 {
 
 	@Test
 	public void Test_FAIL() {
