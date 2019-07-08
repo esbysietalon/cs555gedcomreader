9938a37425c2da0a9b95b5ec506f2ae06e7f7dd7
Nick-Marzullo <nmarzullo@protonmail.ch>
2019-07-07 21:37:19 -0400

    Formatting

--- Test18.java
+++ Test18.java
@@ -6,21 +6,19 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class Test18 {
+    @Test
+    public void Test_US18() {
+    	Main main = new Main();
+	Main.readData("UserStory21.ged");
 
-	@Test
-	public void Test_US18() {
-		Main main = new Main();
-		Main.readData("UserStory21.ged");
+        ArrayList<Family> uso = US18.checkIncest(main.getFamilies());
 
-		ArrayList<Family> uso = US18.checkIncest(main.getFamilies());
+	boolean anomaly = false;
 
-		boolean anomaly = false;
-
-                //Check that the output from US21 is the expected result, that is: 1 result with id @F1@
-		if(!(uso.size()==0)) {
-                    anomaly = true; // if the expected result is not found, flag a problem
-                }
-
-		assertFalse(anomaly);
-	}
+            //Check that the output from US21 is the expected result, that is: 1 result with id @F1@
+	if(!(uso.size()==0)) {
+            anomaly = true; // if the expected result is not found, flag a problem
+        }
+	assertFalse(anomaly);
+    }
 }
