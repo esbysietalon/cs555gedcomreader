e873395b1a62c9843f183a2d91a5de040c7d319e
ebweinberger <elibweinberger@gmail.com>
2019-08-04 17:13:37 -0400

    test 33

---
+++ Test33.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class Test33{
+
+  @Test
+  public void Test_US33(){
+    Main main = new Main();
+    main.readData("UserStory33.ged");
+
+    ArrayList<Individual> orphans = US33.listOrphans(main.getIndividuals());
+    ArrayList<String> required = new ArrayList<String>();
+    required.add("@I13@");
+    required.add("@I8@");
+
+
+    main.compareIDs(orphans, required);
+
+  }
+
+
+}
